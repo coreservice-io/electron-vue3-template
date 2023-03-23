a3ab5ffa0fe887ae47375e0ff0b17c6250634ca0
leo <leo@leos-MacBook-Pro.local>
2023-03-23 15:11:44 +0800

    update

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from "vue-router";
+import Spinner from "@/layouts/overlay/Spinner.vue";
 </script>
 
 <template>
-  <HelloWorld/>
+  <spinner>
+    <RouterView></RouterView>
+  </spinner>
 </template>
-
- 
